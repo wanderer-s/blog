7a776b2126a32334056502eb0f4c6816189a5879
Wanderer-s <ows0871@gmail.com>
2020-09-16 19:28:49 +0900

    gatsby test init

---
+++ config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  siteTitle: 'Wanderer', // <title>
+  manifestName: 'Prologue',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-prologue/`, // This path is subpath of your hosting https://domain/portfolio
+  authorName: 'Wonseop Oh',
+  heading: 'Back-End Developer',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/wanderer-s',
+    },
+    // {
+    //   icon: 'fa-twitter',
+    //   name: 'Twitter',
+    //   url: 'https://twitter.com/onlyanubhav',
+    // },
+    // {
+    //   icon: 'fa-facebook',
+    //   name: 'Facebook',
+    //   url: 'https://facebook.com/theanubhav',
+    // },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:ows0871@gmail.com',
+    },
+  ],
+};
